e420668465582efaaf28a83f2b01c98435f17166
j-rojas80 <jroja80@illinois.edu>
2025-10-06 13:09:07 -0500

    Add files via upload

---
+++ responsive-review/change.js
@@ -0,0 +1,31 @@
+function change01() {
+    document.getElementById("big").src = "images/paris-main.jpg"
+}
+
+function change02() {
+    document.getElementById("big").src = "images/tokyo-main.jpg"
+}
+
+function change03() {
+    document.getElementById("big").src = "images/rome-main.jpg"
+}
+
+function change04() {
+    document.getElementById("big").src = "images/city.jpg"
+}
+
+function change05() {
+    document.getElementById("paris").src = "../images/paris-main.jpg"
+}
+
+function change06() {
+    document.getElementById("paris").src = "../images/tokyo-main.jpg"
+}
+
+function change07() {
+    document.getElementById("paris").src = "../images/rome-main.jpg"
+}
+
+function change08() {
+    document.getElementById("paris").src = "../images/city.jpg"
+}
