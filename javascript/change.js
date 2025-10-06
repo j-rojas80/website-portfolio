100ef1a58ad31c564363ef5b6707d0ac432fefbb
j-rojas80 <jroja80@illinois.edu>
2025-10-06 13:07:32 -0500

    Add files via upload

---
+++ javascript/change.js
@@ -0,0 +1,20 @@
+function change01() {
+    document.getElementById("large").src = "images/autumn.jpg"
+}
+
+function change02() {
+    document.getElementById("large").src = "images/lake.jpg"
+}
+
+function change03() {
+    document.getElementById("large").src = "images/nyc.jpg"
+}
+
+function change04() {
+    document.getElementById("large").src = "images/puppies.jpg"
+}
+
+function change05() {
+    document.getElementById("large").src = "images/sunset.jpg"
+}
+
